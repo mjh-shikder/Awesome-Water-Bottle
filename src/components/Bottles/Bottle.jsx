17e41d644c58fa28c3b0d8895bc983ad148f5976
mjh-shikder <mjhsikder@gmail.com>
2025-09-24 21:15:49 +0600

    button add to cart funtion adde

--- src/components/Bottles/Bottle.jsx
+++ src/components/Bottles/Bottle.jsx
@@ -1,15 +1,18 @@
+// Bottle.jsx
 import React from 'react';
 import './Bottle.css'
 
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCart }) => {
    // console.log(bottle);
-    const {img, price, name } = bottle;
+    const {img, price, name, stock} = bottle;
     
     return (
         <div className='card'>
             <img src={img} alt="" />
             <h2>{name}</h2>
-            <h2>Price: {price} </h2>
+            <h2>Price: ${price} </h2>
+            <p>In Stock: {stock}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Buye Now</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,21 +1,36 @@
-import React, { use } from 'react';
-import Bottle from './Bottle';
-
+// Bottles.jsx
+import React, { use, useState } from "react";
+import Bottle from "./Bottle";
+import "./Bottle.css";
 const Bottles = ({ bottlesPromise }) => {
-    const bottles = use(bottlesPromise);
-   // console.log(bottles);
-    
-    return (
-        <div>
-            <h1>Bottles {bottles.length} </h1>
-        {
-                bottles.map(bottle => <Bottle
-                    key={bottle.id}
-                    bottle={bottle}></Bottle>)     
-        }
+  const [cart, setCart] = useState([]);
 
-        </div>
-    );
+  const bottles = use(bottlesPromise);
+
+  const handleAddToCart = (bottle) => {
+      // console.log("bottle will be adde to the cart", bottle);
+      const newCart = [...cart, bottle];
+      setCart(newCart);
+  };
+  // console.log(bottles);
+
+  return (
+    <div>
+          <h1>Bottles {bottles.length} </h1>
+          <p>Added to cart:{cart.length} </p>
+
+      <div className="bottles-container">
+        {bottles.map((bottle) => (
+            <Bottle
+                key={bottle.id}
+                bottle={bottle}
+                handleAddToCart={handleAddToCart}
+            >
+                </Bottle>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Bottles;
+export default Bottles; 
